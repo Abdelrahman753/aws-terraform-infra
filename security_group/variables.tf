ba7a60cf5369613ce3e2a6cdd599a33496914e94
Abdelrahman Mahmoud <abdelrahman@email.com>
2026-01-22 20:04:09 +0200

    module for security group

---
+++ security_group/variables.tf
@@ -0,0 +1,35 @@
+variable "Security-name" {
+    description = "The name of the security group"
+    type        = string
+}
+variable "vpc_id" {
+    description = "The ID of the VPC"
+    type        = string
+}
+
+variable "ingress_rules" {
+    description = "List of ingress rules for the security group"
+    type        = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = list(string)
+    }))
+  
+}
+
+variable "egress_rules" {
+    description = "List of egress rules for the security group"
+    type        = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = list(string)
+    }))
+}
+
+variable "env" {
+    description = "The environment for the security group"
+    type        = string
+  
+}
